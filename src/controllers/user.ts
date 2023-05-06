c1e2a35a248eb4327982746b95796ed61c3dad1a
Mayopi <cyade007@gmail.com>
2023-05-06 15:07:34 +0700

    add search user by email endpoint

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response, response } from "express";
-import { getUsers } from "../models/User";
+import { getUserByEmail, getUsers } from "../models/User";
 import { responses } from "../lib/response";
 
 export const allUser = async (req: Request, res: Response): Promise<Response> => {
@@ -9,7 +9,42 @@ export const allUser = async (req: Request, res: Response): Promise<Response> =>
       message: "Success",
       status: 200,
       data: users,
+    }).end();
+  } catch (error) {
+    console.log(error);
+    return responses(res, 500, {
+      message: "Failed",
+      status: 500,
+      error: error.message,
     });
+  }
+};
+
+export const user = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { email } = req.params;
+
+    if (!email)
+      return responses(res, 400, {
+        message: "Failed",
+        status: 400,
+        error: "Email is required!",
+      });
+
+    const user = await getUserByEmail(email);
+
+    if (!user)
+      return responses(res, 404, {
+        message: "Failed",
+        status: 404,
+        error: `User with email ${email} is not found!`,
+      });
+
+    return responses(res, 200, {
+      message: "Success",
+      status: 200,
+      data: user,
+    }).end();
   } catch (error) {
     console.log(error);
     return responses(res, 500, {
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { allUser } from "../controllers/user";
+import { allUser, user } from "../controllers/user";
 import { checkAuth } from "../middlewares/auth";
 
 export default (router: Router): void => {
   router.get("/api/users", checkAuth, allUser);
+  router.get("/api/users/:email", checkAuth, user);
 };
